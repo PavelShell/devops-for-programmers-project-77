57cb58c338cdf0844441edc0505204771c43bfe4
Pavel Shalianhouski <p.shelengovsky@gmail.com>
2023-10-01 19:36:30 +0200

    Implemented Terraform dieployment of the app

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,20 +1,83 @@
-resource "digitalocean_droplet" "web1" {
-  name   = "web-1"
-  size   = "s-1vcpu-1gb"
-  image  = "ubuntu-20-04-x64"
-  region = "nyc3"
+resource "digitalocean_database_cluster" "postgres" {
+  name       = "db"
+  engine     = "pg"
+  version    = "14"
+  size       = "db-s-1vcpu-1gb"
+  region     = "fra1"
+  node_count = 1
 }
 
-resource "digitalocean_droplet" "web2" {
-  name   = "web-1"
-  size   = "s-1vcpu-1gb"
-  image  = "ubuntu-20-04-x64"
-  region = "nyc3"
+data "digitalocean_ssh_key" "default" {
+  name = "Home PC key"
 }
 
-resource "digitalocean_loadbalancer" "public" {
-  name   = "loadbalancer-1"
-  region = "nyc3"
+data "local_file" "private_key" {
+  filename = "keys/id_rsa"
+}
+
+resource "digitalocean_droplet" "app_node_1" {
+  name   = "node-1"
+  size   = "s-1vcpu-1gb"
+  image  = "docker-20-04"
+  region = "fra1"
+  depends_on = [digitalocean_database_cluster.postgres]
+	ssh_keys = [data.digitalocean_ssh_key.default.fingerprint]
+  
+  provisioner "remote-exec" {
+  connection {
+    type        = "ssh"
+    user        = "root"
+    host        = self.ipv4_address
+    private_key = data.local_file.private_key.content
+  }
+  
+  inline = [
+<<EOT
+sudo docker run -d -p 80:3000 \
+  -e REDMINE_DB_DATABASE=${digitalocean_database_cluster.postgres.database} \
+  -e REDMINE_DB_POSTGRES=${digitalocean_database_cluster.postgres.host} \
+  -e REDMINE_DB_PORT=${digitalocean_database_cluster.postgres.port} \
+  -e REDMINE_DB_USERNAME=${digitalocean_database_cluster.postgres.user} \
+  -e REDMINE_DB_PASSWORD=${digitalocean_database_cluster.postgres.password} \
+  redmine
+EOT
+    ]
+  }
+}
+
+resource "digitalocean_droplet" "app_node_2" {
+  name   = "node-2"
+  size   = "s-1vcpu-1gb"
+  image  = "docker-20-04"
+  region = "fra1"
+  depends_on = [digitalocean_database_cluster.postgres]
+	ssh_keys = [data.digitalocean_ssh_key.default.fingerprint]
+  
+  provisioner "remote-exec" {
+  connection {
+    type        = "ssh"
+    user        = "root"
+    host        = self.ipv4_address
+    private_key = data.local_file.private_key.content
+  }
+  
+  inline = [
+<<EOT
+sudo docker run -d -p 80:3000 \
+  -e REDMINE_DB_DATABASE=${digitalocean_database_cluster.postgres.database} \
+  -e REDMINE_DB_POSTGRES=${digitalocean_database_cluster.postgres.host} \
+  -e REDMINE_DB_PORT=${digitalocean_database_cluster.postgres.port} \
+  -e REDMINE_DB_USERNAME=${digitalocean_database_cluster.postgres.user} \
+  -e REDMINE_DB_PASSWORD=${digitalocean_database_cluster.postgres.password} \
+  redmine
+EOT
+    ]
+  }
+}
+
+resource "digitalocean_loadbalancer" "load_balancer" {
+  name   = "loadbalancer"
+  region = "fra1"
 
   forwarding_rule {
     entry_port     = 80
@@ -25,10 +88,11 @@ resource "digitalocean_loadbalancer" "public" {
   }
 
   healthcheck {
-    port     = 22
-    protocol = "tcp"
+    port     = 80
+    path     = "/"
+    protocol = "http"
   }
 
-  droplet_ids = [digitalocean_droplet.web1.id, digitalocean_droplet.web2.id]
+  droplet_ids = [digitalocean_droplet.app_node_1.id, digitalocean_droplet.app_node_2.id]
 }
  
