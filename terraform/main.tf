5e33fab8a1c594c64eaa1d9061c9f1acc8aff7ac
Pavel Shalianhouski <p.shelengovsky@gmail.com>
2023-10-03 21:41:11 +0200

    Added Ansible aplication deployment configurations

--- terraform/main.tf
+++ terraform/main.tf
@@ -22,27 +22,6 @@ resource "digitalocean_droplet" "app_node_1" {
   region = "fra1"
   depends_on = [digitalocean_database_cluster.postgres]
 	ssh_keys = [data.digitalocean_ssh_key.default.fingerprint]
-  
-  provisioner "remote-exec" {
-  connection {
-    type        = "ssh"
-    user        = "root"
-    host        = self.ipv4_address
-    private_key = data.local_file.private_key.content
-  }
-  
-  inline = [
-<<EOT
-sudo docker run -d -p 80:3000 \
-  -e REDMINE_DB_DATABASE=${digitalocean_database_cluster.postgres.database} \
-  -e REDMINE_DB_POSTGRES=${digitalocean_database_cluster.postgres.host} \
-  -e REDMINE_DB_PORT=${digitalocean_database_cluster.postgres.port} \
-  -e REDMINE_DB_USERNAME=${digitalocean_database_cluster.postgres.user} \
-  -e REDMINE_DB_PASSWORD=${digitalocean_database_cluster.postgres.password} \
-  redmine
-EOT
-    ]
-  }
 }
 
 resource "digitalocean_droplet" "app_node_2" {
@@ -52,27 +31,6 @@ resource "digitalocean_droplet" "app_node_2" {
   region = "fra1"
   depends_on = [digitalocean_database_cluster.postgres]
 	ssh_keys = [data.digitalocean_ssh_key.default.fingerprint]
-  
-  provisioner "remote-exec" {
-  connection {
-    type        = "ssh"
-    user        = "root"
-    host        = self.ipv4_address
-    private_key = data.local_file.private_key.content
-  }
-  
-  inline = [
-<<EOT
-sudo docker run -d -p 80:3000 \
-  -e REDMINE_DB_DATABASE=${digitalocean_database_cluster.postgres.database} \
-  -e REDMINE_DB_POSTGRES=${digitalocean_database_cluster.postgres.host} \
-  -e REDMINE_DB_PORT=${digitalocean_database_cluster.postgres.port} \
-  -e REDMINE_DB_USERNAME=${digitalocean_database_cluster.postgres.user} \
-  -e REDMINE_DB_PASSWORD=${digitalocean_database_cluster.postgres.password} \
-  redmine
-EOT
-    ]
-  }
 }
 
 resource "digitalocean_loadbalancer" "load_balancer" {
